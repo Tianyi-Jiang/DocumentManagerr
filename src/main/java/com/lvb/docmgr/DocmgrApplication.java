bb24799f9846b2199a6faf1131993a7f25d71a12
cmc08 <cmc0817@126.com>
2018-07-04 15:15:29 +0800

    Initial Commit

---
+++ src/main/java/com/lvb/docmgr/DocmgrApplication.java
@@ -0,0 +1,12 @@
+package com.lvb.docmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocmgrApplication.class, args);
+	}
+}
